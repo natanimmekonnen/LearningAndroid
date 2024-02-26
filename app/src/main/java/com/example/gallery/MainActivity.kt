0647dcbd578ab64e5e39216e5ed733937c9e4591
Natanim Mekonnen <mekonnennatanim1@gmail.com>
2024-02-26 21:27:56 +0300

    got the ui

--- app/src/main/java/com/example/gallery/MainActivity.kt
+++ app/src/main/java/com/example/gallery/MainActivity.kt
@@ -62,12 +62,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun AllCombined(modifier: Modifier= Modifier.fillMaxSize())
+fun AllCombined(modifier: Modifier= Modifier)
 {
-    Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center, modifier = modifier.wrapContentSize()) {
-        ImageDisplay(image= R.drawable._168c8608_43f4_4124_8095_33ac54eedb1f)
+    Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.SpaceBetween, modifier = modifier.fillMaxSize()) {
+        Spacer(modifier = Modifier.size(80.dp))
+       Surface(shadowElevation =6.dp, color= Color(0xffffefdd), modifier = Modifier.padding(20.dp)) {
+
+           ImageDisplay(image= R.drawable._168c8608_43f4_4124_8095_33ac54eedb1f)
+       }
+        Spacer(modifier = Modifier.size(50.dp))
         DescriptionOfArt(description = R.string.grey_brown_girlie_engoying_life, date =R.string.bing_ai_2024 )
-        ButtonDisplay()
+
+        ButtonDisplay(modifier=Modifier)
     }
 
 }
@@ -77,15 +83,14 @@ fun AllCombined(modifier: Modifier= Modifier.fillMaxSize())
 fun ImageDisplay(modifier:Modifier=Modifier, @DrawableRes image: Int)
 {
     Column( horizontalAlignment =Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center,
-        modifier = modifier.background(Color( 0xffffefdd))
     )
     {
         Image(
             painter= painterResource(id = image),
             null,
             modifier = Modifier
-                .padding(30.dp)
-              //  .border( width = 2.dp,shape= RoundedCornerShape(20.dp), color =Color(0xffffefdd) )
+                .height(380.dp)
+                .padding(25.dp)
 
         )
 
@@ -97,14 +102,18 @@ fun ImageDisplay(modifier:Modifier=Modifier, @DrawableRes image: Int)
 @Composable
 fun DescriptionOfArt(modifier:Modifier=Modifier,   @StringRes description: Int, @StringRes date: Int)
 {
-    Column( verticalArrangement = Arrangement.Bottom,
-        modifier = modifier.background(Color( 0xffffefdd))
-            .fillMaxHeight(0.8f)){
+    Column( verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .background(Color(0xffffefdd))
+            .height(100.dp)
+            .padding(25.dp)
+            ){
         Text(text = stringResource(description),
             fontFamily = FontFamily.SansSerif,
-            fontWeight = FontWeight.SemiBold
+            fontSize = 20.sp
         )
         Text(text= stringResource(date),
+            fontWeight = FontWeight.SemiBold,
             fontSize = 12.sp)
 
 
@@ -114,14 +123,20 @@ fun DescriptionOfArt(modifier:Modifier=Modifier,   @StringRes description: Int, 
 @Composable
 fun ButtonDisplay(modifier:Modifier=Modifier)
 {
-    Column(  modifier = modifier.fillMaxHeight(0.9f), verticalArrangement = Arrangement.Bottom) {
-        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceEvenly)
+
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp), verticalAlignment = Alignment.Bottom, horizontalArrangement = Arrangement.spacedBy(25.dp))
         {
             Button(
                 onClick = { /*TODO*/ },
-                colors =  ButtonDefaults.buttonColors(containerColor = Color( 0xffffefdd), contentColor = Color.Black),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xffffefdd),
+                    contentColor = Color.Black
+                ),
                 modifier = Modifier
-                    .width(120.dp)
+                    .width(160.dp)
                     .height(38.dp),
             ) {
                 Text(
@@ -130,30 +145,32 @@ fun ButtonDisplay(modifier:Modifier=Modifier)
                 )
 
             }
-            Button(onClick = { /*TODO*/ },
-                colors =  ButtonDefaults.buttonColors(containerColor = Color( 0xffffefdd), contentColor = Color.Black),
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xffffefdd),
+                    contentColor = Color.Black
+                ),
                 modifier = Modifier
-                .width(120.dp)
-                .height(38.dp)) {
+                    .width(160.dp)
+                    .height(38.dp)
+            ) {
                 Text(
                     text = "Next",
                     fontSize = 18.sp
                 )
 
             }
-
         }
-    }
+        }
 
-}
+
 @Preview(showBackground = true)
 @Composable
 fun ImageDisplayPreview()
 {
     GalleryTheme {
-//        ImageDisplay(image= R.drawable._168c8608_43f4_4124_8095_33ac54eedb1f)
-    //    DescriptionOfArt(description = R.string.grey_brown_girlie_engoying_life, date =R.string.bing_ai_2024 )
-      //  ButtonDisplay( )
+
         AllCombined()
     }
 
